6414237f5d9436aeb94d0d0ec99ed1fab2056c80
Artur Fayzullin <fayzullin.ar@mail.ru>
2024-11-29 14:57:32 +0300

    add project

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,41 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+
+@Command(name = "gendiff",
+        version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.",
+        mixinStandardHelpOptions = true)
+public class App implements Runnable {
+
+    @Parameters(index = "0", description = "The file whose checksum to calculate.")
+    private File filepath1;
+
+    @Parameters(index = "1", description = "The file whose checksum to calculate.")
+    private File filepath2;
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format", defaultValue = "stylish",
+            description = "output format [default: ${DEFAULT-VALUE}]")
+    private String format;
+
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    boolean helpRequested;
+
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    boolean versionInfoRequested;
+
     public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public void run() {
         System.out.println("Hello World!");
     }
 }
