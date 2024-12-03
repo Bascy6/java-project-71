cf06b8a3b4227df4349dc487ade859873bcd238b
Artur Fayzullin <fayzullin.ar@mail.ru>
2024-12-03 20:20:21 +0300

    add differ

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,10 +5,13 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.util.concurrent.Callable;
+
 @Command(name = "gendiff",
         description = "Compares two configuration files and shows a difference.",
         mixinStandardHelpOptions = true)
-public class App implements Runnable {
+
+public class App implements Callable<Integer> {
 
     @Parameters(index = "0", description = "The first file to compare.")
     private String filepath1;
@@ -29,13 +32,15 @@ public class App implements Runnable {
     }
 
     @Override
-    public void run() {
-        Parsing parsing = new Parsing();
+    public Integer call() throws Exception {
+        Differ differ = new Differ();
         try {
-            Config mergedConfig = parsing.mergeConfigs(filepath1, filepath2);
-            System.out.println("Parsing: " + mergedConfig);
+            String result = differ.generate(filepath1, filepath2);
+            System.out.println(result);
+            return 0;
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
+            return 1;
         }
     }
 }
