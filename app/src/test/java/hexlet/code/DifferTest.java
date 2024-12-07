cc98651a2d6f70bcb009e777a9743e4cd5ddb084
Artur Fayzullin <fayzullin.ar@mail.ru>
2024-12-07 23:06:08 +0300

    add changes

---
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -0,0 +1,75 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DifferTest {
+
+    @Test
+    public void testGenerate() throws IOException {
+        String filePath1 = "file1.json";
+        String filePath2 = "file2.json";
+
+        String expected = "{\n"
+                + "  - follow: false\n"
+                + "    host: hexlet.io\n"
+                + "  - proxy: 123.234.53.22\n"
+                + "  - timeout: 50\n"
+                + "  + timeout: 20\n"
+                + "  + verbose: true\n"
+                + "}";
+
+        String actual = Differ.generateDiff(filePath1, filePath2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGenerateWithEmptyFile() throws IOException {
+        String filePath1 = "file1.json";
+        String filePath2 = "empty.json";
+
+        String expected = "{\n"
+                + "  - follow: false\n"
+                + "  - host: hexlet.io\n"
+                + "  - proxy: 123.234.53.22\n"
+                + "  - timeout: 50\n"
+                + "}";
+
+        String actual = Differ.generateDiff(filePath1, filePath2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGenerateWithIdenticalFiles() throws IOException {
+        String filePath1 = "file1.json";
+        String filePath2 = "file1.json";
+
+        String expected = "{\n"
+                + "    follow: false\n"
+                + "    host: hexlet.io\n"
+                + "    proxy: 123.234.53.22\n"
+                + "    timeout: 50\n"
+                + "}";
+
+        String actual = Differ.generateDiff(filePath1, filePath2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGenerateWithDifferentKeys() throws IOException {
+        String filePath1 = "file1.json";
+        String filePath2 = "file3.json";
+
+        String expected = "{\n"
+                + "  - follow: false\n"
+                + "  - host: hexlet.io\n"
+                + "  - proxy: 123.234.53.22\n"
+                + "  - timeout: 50\n"
+                + "  + key3: value3\n"
+                + "}";
+
+        String actual = Differ.generateDiff(filePath1, filePath2);
+        assertEquals(expected, actual);
+    }
+}
