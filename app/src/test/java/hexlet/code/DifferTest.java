d2812390e03966dcb0d3745ef152fa0921a4c645
Artur Fayzullin <fayzullin.ar@mail.ru>
2024-12-15 20:11:05 +0300

    add step 11

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -78,17 +78,29 @@ public class DifferTest {
     }
 
     @Test
-    public void testUnsupportedFileExtension() {
-        String filePath1 = "src/test/resources/file1.txt";
-        String filePath2 = "src/test/resources/file2.json";
-        assertThrows(IllegalArgumentException.class, () -> Differ.generateDiff(filePath1, filePath2, "stylish"));
+    public void testEmptyFiles() throws Exception {
+        String filePath1 = "src/test/resources/empty_file1.json";
+        String filePath2 = "src/test/resources/empty_file2.json";
+        String expected = normalize(Files.readString(Paths.get("src/test/resources/expected/expected_empty.txt")));
+        String result = normalize(Differ.generateDiff(filePath1, filePath2, "stylish"));
+        assertEquals(expected, result);
     }
 
     @Test
-    public void testDifferentFormats() throws Exception {
+    public void testIdenticalFiles() throws Exception {
         String filePath1 = "src/test/resources/file1.json";
-        String filePath2 = "src/test/resources/file2.yml";
-        String expected = normalize(Files.readString(Paths.get("src/test/resources/expected/expected_stylish.txt")));
+        String filePath2 = "src/test/resources/file1.json";
+        String expected = normalize(Files.readString(Paths.get("src/test/resources/expected/expected_identical.txt")));
+        String result = normalize(Differ.generateDiff(filePath1, filePath2, "stylish"));
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testMinimalChange() throws Exception {
+        String filePath1 = "src/test/resources/file1.json";
+        String filePath2 = "src/test/resources/file1_minimal_change.json";
+        String expected = normalize(
+                Files.readString(Paths.get("src/test/resources/expected/expected_minimal_change.txt")));
         String result = normalize(Differ.generateDiff(filePath1, filePath2, "stylish"));
         assertEquals(expected, result);
     }
