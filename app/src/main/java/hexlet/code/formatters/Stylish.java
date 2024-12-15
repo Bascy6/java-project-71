08d4f04b6abc86830c3358aa52c3ab9784b22197
Artur Fayzullin <fayzullin.ar@mail.ru>
2024-12-15 22:33:59 +0300

    Changes

--- app/src/main/java/hexlet/code/formatters/Stylish.java
+++ app/src/main/java/hexlet/code/formatters/Stylish.java
@@ -31,7 +31,7 @@ public class Stylish {
                     result.append("  - ").append(key).append(": ").append(formatValue(value2)).append("\n");
                     result.append("  + ").append(key).append(": ").append(formatValue(value1)).append("\n");
                 } else if (value1 != null && !value1.equals(value2)) {
-                    result.append("  - ").append(key).append(": ").append(formatValue(value2)).append("\n");
+                    result.append("  - ").append(key).append(": ").append(formatValue(value1)).append("\n");
                     result.append("  + ").append(key).append(": ").append(formatValue(value2)).append("\n");
                 } else if (value1.equals(value2)) {
                     result.append("    ").append(key).append(": ").append(formatValue(value1)).append("\n");
